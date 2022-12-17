2f6f1040e960178f1ec49e0ed3a4afb478548fae
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2022-12-17 11:34:37 +0400

    Test

--- src/test/java/ru/salamon/hiber/HiberApplicationTests.java
+++ src/test/java/ru/salamon/hiber/HiberApplicationTests.java
@@ -3,11 +3,14 @@ package ru.salamon.hiber;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class HiberApplicationTests {
 
     @Test
-    void contextLoads() {
+    void test() {
+        System.out.println("##teamcity[testMetadata testName='package.Test.testName' name='setUp time' value='+ /usesnpe/UsesRedundant.php:67 WEAK WARNING (int) 'Type cast is redundant'']");
+        assertTrue(false);
     }
 
 }
