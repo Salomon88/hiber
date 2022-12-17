39238e85ef09cf8f07462e9c555e6f00186911bf
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2022-12-17 11:55:19 +0400

    Test

--- src/test/java/ru/salamon/hiber/HiberApplicationTests.java
+++ src/test/java/ru/salamon/hiber/HiberApplicationTests.java
@@ -1,15 +1,20 @@
 package ru.salamon.hiber;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 class HiberApplicationTests {
 
     @Test
     void test() {
-        System.out.println("##teamcity[testMetadata testName='package.Test.testName' name='setUp time' value='+ /usesnpe/UsesRedundant.php:67 WEAK WARNING (int) 'Type cast is redundant'']");
+        log.info("##teamcity[testStarted name='HiberApplicationTests']");
+        log.info("##teamcity[testMetadata testName='HiberApplicationTests' name='setUp time' value='+ /usesnpe/UsesRedundant.php:67 WEAK WARNING (int) 'Type cast is redundant'']");
+        log.info("##teamcity[testFailed name='HiberApplicationTests' message='failure message' details='message and stack trace']");
+        log.info("##teamcity[testFinished name='HiberApplicationTests]");
         assertTrue(false);
     }
 
